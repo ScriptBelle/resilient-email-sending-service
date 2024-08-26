cd9342caa39be7e9af392b418f6345a666ed8261
ScriptBelle <zainabdurrani882@gmail.com>
2024-08-26 17:44:08 +0530

    Update main.js
    This file contains instances of 'MockEmailProvider' and 'EmailService'.
    It also performs an email sending action

---
+++ PearlT/email-service/main.js
@@ -0,0 +1,10 @@
+// src/index.js
+
+const { EmailService, MockEmailProvider } = require('./services/EmailService');
+
+const provider1 = new MockEmailProvider('Provider1');
+const provider2 = new MockEmailProvider('Provider2');
+const emailService = new EmailService([provider1, provider2]);
+
+const email = { id: '1234', to: 'example@test.com', subject: 'Test Email', body: 'Hello World!' };
+emailService.sendEmail(email).then(result => console.log(result));
